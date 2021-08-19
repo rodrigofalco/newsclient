d7d4e05005d24472d234799ab044a6ec6fede566
Rodrigo I Falco <rfalco@patagonian.it>
2021-08-19 19:41:05 -0300

    Api and react query.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,25 @@
+import { useQuery } from 'react-query';
+import { getNews } from '../services/api';
+
 export default function HomePage() {
-  return 'Home';
+  const { isLoading, isError, data, error } = useQuery('homeNews', getNews);
+
+  if (isLoading) {
+    return <span>Loading...</span>;
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
+  console.log(data);
+
+  // We can assume by this point that `isSuccess === true`
+
+  return (
+    <ul>
+      {data.articles.map((todo) => (
+        <li key={todo.id}>{todo.title}</li>
+      ))}
+    </ul>
+  );
 }
---
+++ src/services/api.js
@@ -0,0 +1,11 @@
+const API_KEY = 'f7ddb6a3dc874c77ae95187906adeb6c';
+const API_BASE_URL = 'https://newsapi.org/v2';
+const country = 'AR';
+const category = 'business';
+
+export async function getNews() {
+  //https://newsapi.org/v2/top-headlines?country=us&category=business&
+  return fetch(
+    `${API_BASE_URL}/top-headlines?country=${country}&category=${category}&apiKey=${API_KEY}`
+  ).then((res) => res.json());
+}
