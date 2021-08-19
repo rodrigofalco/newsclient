f97a448e7951b3ffe72f3057e26c274f9bfb8a5e
Rodrigo I Falco <rfalco@patagonian.it>
2021-08-19 19:27:33 -0300

    Added material UI

--- src/App.js
+++ src/App.js
@@ -5,25 +5,13 @@ import './App.css';
 import HomePage from './pages/Home';
 import CategoryPage from './pages/Category';
 import NewsPage from './pages/News';
+import NavBar from './components/NavBar';
 
 export default function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/category/business">Business</Link>
-            </li>
-            <li>
-              <Link to="/category/sports">Sports</Link>
-            </li>
-          </ul>
-        </nav>
-
+        <NavBar />
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
